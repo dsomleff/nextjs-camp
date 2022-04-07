47337959d4c99c8678e5fe534c23302760433c9b
Dmytro Somliev <77507444+dsomleff@users.noreply.github.com>
2022-04-07 15:26:10 +0200

    Prepare a New Project

--- summary-app/pages/news/[newsId].js
+++
@@ -1,8 +0,0 @@
-import { useRouter } from "next/router";
-
-export default function NewsDetails() {
-    const router = useRouter();
-    const newsId = router.query.newsId;
-    
-    return <h1>{newsId}</h1>;
-}
