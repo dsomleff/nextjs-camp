91f6a92e1c628d48c98d6bfad0f4ef4b7787ff6d
Dmytro Somliev <77507444+dsomleff@users.noreply.github.com>
2022-04-08 13:22:30 +0200

    MeetupDetail Component/CSS

---
+++ summary-app/pages/[meetupId]/index.js
@@ -0,0 +1,12 @@
+import MeetupDetail from "../../components/meetups/MeetupDetail";
+
+export default function MeetupDetails() {
+    return (
+      <MeetupDetail 
+        image='https://upload.wikimedia.org/wikipedia/commons/8/87/Potemkinstairs.jpg'
+        title='hello'
+        address='street'
+        description='hello'
+      />
+    );
+}
