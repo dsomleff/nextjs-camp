f5b6ec84ce756c8772305eb44aa7843c8c2a5ce5
Dmytro Somliev <77507444+dsomleff@users.noreply.github.com>
2022-04-15 10:26:05 +0200

    Fix Head issue

--- summary-app/pages/index.js
+++ summary-app/pages/index.js
@@ -1,6 +1,6 @@
 import MeetupList from "../components/meetups/MeetupList";
 import {Fragment} from "react";
-import {Head} from "next/document";
+import Head from "next/head";
 
 const DUMMY_MEETUPS = [
     {
